14b9487047a1bcaf3b55a763ce4bbb1565c5cfb9
eschnack0 <eschnack@hotmail.com>
2020-03-08 13:21:24 -0400

    Initial check in

---
+++ src/main/java/com/bs/sonar/Hello/App.java
@@ -0,0 +1,13 @@
+package com.bs.sonar.Hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
